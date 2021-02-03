30227e1931fb3f8da1e9d5401cf332b74c9c4d2c
Maarten Coppens <martinuske@gmail.com>
2021-02-03 11:56:41 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  #app
+    Recorder
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Recorder from './components/Recorder.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
-};
+    Recorder,
+  },
+}
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
 }
 </style>
---
+++ src/components/Recorder.vue
@@ -0,0 +1,170 @@
+<template lang="pug">
+  .container
+    .row
+      a.button(
+        @click="record"
+        :class="`shadow-mic-${analyserPercentage}`"
+        )
+        svg-icon(
+          type="mdi"
+          :path="mdiMicrophone"
+          :size="50"
+          :style="`color: ${recording ? 'red' : 'darkslategrey'}`"
+          )
+    .row
+      p {{ timer || '00:00:00:000' }}
+</template>
+
+<script>
+import RecordRTC from 'recordrtc'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiMicrophone } from '@mdi/js'
+import { ipcRenderer } from 'electron'
+
+export default {
+  name: 'Recorder',
+  data() {
+    return {
+      mdiMicrophone,
+      recording: false,
+      recorder: null,
+      stream: null,
+      timer: null,
+      analyserPercentage: null,
+      timerInterval: null,
+      analyserInterval: null,
+    }
+  },
+  methods: {
+    async record() {
+      if (this.recording) {
+        this.recording = false
+        this.recorder.stopRecording(() => {
+          this.stream.getTracks().forEach(track => {
+            track.stop()
+          })
+
+          this.recorder.getDataURL(async dataURL => {
+            const file = {
+              audio: {
+                type: this.recorder.getBlob().type || 'audio/wav',
+                dataURL,
+              },
+            }
+
+            console.log(file)
+
+            ipcRenderer.send('file-save', file)
+          })
+
+          clearInterval(this.analyserInterval)
+          clearInterval(this.timerInterval)
+          this.analyserPercentage = 0
+        })
+        return
+      }
+
+      this.stream = await navigator.mediaDevices.getUserMedia({
+        audio: { echoCancellation: true },
+      })
+
+      this.analyser()
+      this.startTime = new Date()
+      this.timerFn()
+
+      this.recorder = RecordRTC(this.stream, {
+        type: 'audio',
+
+        mimeType: 'audio/webm',
+        sampleRate: 44100,
+        // used by StereoAudioRecorder
+        // the range 22050 to 96000.
+        // let us force 16khz recording:
+        desiredSampRate: 16000,
+
+        numberOfAudioChannels: 1,
+
+        timeSlice: 1000,
+      })
+
+      this.recorder.startRecording()
+      this.recording = true
+    },
+    analyser() {
+      const audioContext = new AudioContext()
+      const audioSource = audioContext.createMediaStreamSource(this.stream)
+      const analyser = audioContext.createAnalyser()
+      analyser.fftSize = 512
+      analyser.minDecibels = -127
+      analyser.maxDecibels = 0
+      analyser.smoothingTimeConstant = 0.4
+      audioSource.connect(analyser)
+      const volumes = new Uint8Array(analyser.frequencyBinCount)
+      this.analyserInterval = setInterval(() => {
+        analyser.getByteFrequencyData(volumes)
+        let volumeSum = 0
+        for (const volume of volumes) volumeSum += volume
+        const averageVolume = volumeSum / volumes.length
+        // Value range: 127 = analyser.maxDecibels - analyser.minDecibels;
+        this.analyserPercentage = Math.floor((averageVolume * 100) / 127)
+      }, 100)
+    },
+    timerFn() {
+      this.timerInterval = setInterval(() => {
+        let difference = new Date().getTime() - this.startTime.getTime()
+
+        let hours = Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+        )
+        let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+        let seconds = Math.floor((difference % (1000 * 60)) / 1000)
+        let milliseconds = Math.floor((difference % (1000 * 60)) / 10)
+        hours = hours < 10 ? '0' + hours : hours
+        minutes = minutes < 10 ? '0' + minutes : minutes
+        seconds = seconds < 10 ? '0' + seconds : seconds
+        milliseconds =
+          milliseconds < 100
+            ? milliseconds < 10
+              ? '00' + milliseconds
+              : '0' + milliseconds
+            : milliseconds
+        this.timer = hours + ':' + minutes + ':' + seconds + ':' + milliseconds
+      }, 10)
+    },
+  },
+  components: {
+    SvgIcon,
+  },
+}
+</script>
+
+<style lang="scss">
+.container {
+  font-family: 'Courier New', Courier, monospace;
+  display: flex;
+  flex-flow: row wrap;
+}
+.row {
+  display: flex;
+  justify-content: center;
+  flex-basis: 100%;
+  width: 100%;
+}
+.button {
+  margin: 50px;
+  padding: 25px;
+  border-radius: 50%;
+  background-color: whitesmoke;
+  box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  transition: box-shadow 0.1s ease-in;
+}
+
+@for $i from 1 to 100 {
+  .shadow-mic-#{$i} {
+    -webkit-box-shadow: 0px 0px 1px #{$i / 2}px rgba(224, 127, 127, 0.3);
+    -moz-box-shadow: 0px 0px 1px #{$i / 2}px rgba(224, 127, 127, 0.3);
+    box-shadow: 0px 0px 1px #{$i / 2}px rgba(224, 127, 127, 0.3);
+  }
+}
+</style>
